3e7a2575ecbc567863cc14a86a6aa12ba4717072
NataliaOsejo <natalia.osejoh@utadeo.edu.co>
2024-10-24 11:42:30 -0500

    union y adelanto de dos palabras

--- app/src/main/java/com/example/qta2/IniciarSesion.kt
+++ app/src/main/java/com/example/qta2/IniciarSesion.kt
@@ -1,4 +1,22 @@
 package com.example.qta2
 
-class IniciarSesion {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class IniciarSesion : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.iniciar_sesion)
+
+        // Acción de volver 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_sign_in)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
 }
--- app/src/main/java/com/example/qta2/MainActivity.kt
+++ app/src/main/java/com/example/qta2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.qta2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // boton de inicio de sesion
+        val boton_iniciar_sesion = findViewById<Button>(R.id.button_iniciar_sesion)
+        boton_iniciar_sesion.setOnClickListener {
+            val intent = Intent(this, IniciarSesion::class.java)
+            startActivity(intent)
         }
     }
 }
